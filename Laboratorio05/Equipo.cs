fcfacdfa7eba02a3cde84f5eb39b11da8ed3a268
Yipieight <57154305+Yipieight@users.noreply.github.com>
2023-04-15 00:00:00 -0600

    ar

--- Laboratorio05/Equipo.cs
+++ Laboratorio05/Equipo.cs
@@ -8,8 +8,19 @@ namespace Laboratorio05
 {
     public class Equipo
     {
+        private string nombre;
+        private int partidosGanados;
+        private int partidosEmpatados;
+        private int partidosPerdidos;
+        private int golesFavor;
+        private int golesContra;
         public Equipo(string nombre, int partidosGanados, int partidosEmpatados, int partidosPerdidos, int golesFavor, int golesContra)
         {
+            this.nombre = nombre;
+            this.partidosGanados = partidosGanados;
+            this.partidosEmpatados = partidosEmpatados;
+            this.golesFavor = golesFavor;
+            this.golesContra = golesContra;
         }
 
         public string GetNombre()
