c71b690c41c57843ebd0d897615b30d12bf31a2a
Yipieight <57154305+Yipieight@users.noreply.github.com>
2023-04-23 18:25:43 -0600

    Ejercicios resueltos del laboratorio 5

--- Laboratorio05/Torneo.cs
+++ Laboratorio05/Torneo.cs
@@ -1,17 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Laboratorio05
+﻿namespace Laboratorio05
 {
     public class Torneo
     {
         public static Equipo[][] SimularTorneo(Equipo[] equipos)
         {
+            if (equipos.Length < 2 || !isPowerOf2(equipos.Length))
+            {
+                throw new Exception("La cantidad de equipos debe ser una potencia de dos y deben existir al menos dos equipos");
 
-            return null;
+            }
+            int newSize = (int)Math.Log2(equipos.Length) + 1;
+
+            Equipo[][] resultado = new Equipo[newSize][];
+
+            resultado[0] = equipos;
+
+            int equiposRestantes = equipos.Length;
+            int fase = 1;
+
+            while (equiposRestantes > 1)
+            {
+                resultado[fase] = new Equipo[equiposRestantes / 2];
+
+                for (int i = 0; i < equiposRestantes / 2; i++)
+                {
+                    Partido partido = new Partido(resultado[fase - 1][i], resultado[fase - 1][equiposRestantes - i - 1]);
+                    Equipo ganador = partido.SeleccionarEquipoGanador();
+                    resultado[fase][i] = ganador;
+                }
+
+                equiposRestantes = equiposRestantes / 2;
+
+                fase++;
+            }
+
+            return resultado;
+        }
+
+        private static bool isPowerOf2(int x)
+        {
+            double log2 = Math.Log2(x);
+
+            return (log2 - (int)log2) == 0.0;
+        }
+
+        private static Equipo[] copyArray(Equipo[] equipos)
+        {
+            Equipo[] copy = new Equipo[equipos.Length];
+
+            for (int i = 0; i < equipos.Length; i++)
+            {
+                copy[i] = equipos[i];
+            }
+
+            return copy;
         }
     }
 }
